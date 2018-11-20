1793fb96666764dc0a4e372ca3d17b801b6ef598
공정훈 <rhdtl78@gmail.com>
2018-11-20 23:34:24 +0900

    openface를 이용한 안면인식 서버 분리
    기존 face-detection 에서 서버 디렉토리를 분리해 새로운 repo로 관리

---
+++ openface/infer.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/root/face-detection/server/openface/demos/classifier.py infer /root/face-detection/server/openface/generated-embeddings/classifier.pkl /root/face-detection/server/inferImages/image.jpg
