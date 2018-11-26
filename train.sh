63a7a6ce97da6b3a431f46c548a26025e2eec772
공정훈 <rhdtl78@gmail.com>
2018-11-27 02:27:34 +0900

    학습 데이터 추가

---
+++ train.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+./openface/util/align-dlib.py ./trainingImages/ align outerEyesAndNose ./openface/aligned-images/ --size 96
+./openface/batch-represent/main.lua -outDir ./openface/generated-embeddings/ -data ./openface/aligned-images/
+./openface/demos/classifier.py train ./openface/generated-embeddings/
