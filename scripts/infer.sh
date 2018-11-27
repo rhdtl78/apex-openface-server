f0e6d6e6db11e4912463f12563b616a892363e7f
공정훈 <rhdtl78@gmail.com>
2018-11-27 15:50:34 +0900

    스크립트 수정

--- scripts/infer.sh
+++ scripts/infer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-set DIR=/root/apex-openface-server
+DIR=/root/apex-openface-server
 
 $DIR/openface/demos/classifier.py infer $DIR/openface/generated-embeddings/classifier.pkl $DIR/inferImages/image.jpg
