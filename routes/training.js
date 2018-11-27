edf9cca3f584840a641989534db7e71033f3b027
공정훈 <rhdtl78@gmail.com>
2018-11-28 03:23:07 +0900

    test

--- routes/training.js
+++ routes/training.js
@@ -6,20 +6,24 @@ const shell = require("shelljs");
 require("date-utils");
 
 router.post("/", function(req, res, next) {
-  const { name } = req.body;
-  try {
-    shell.exec(`scripts/train.sh ./inferImages/image.jpg ${name}`, function(code, stdout, stderr) {
-      if (stderr) throw stderr;
-      const Log = new Logs({
-        name: name,
-        confidence: result.confidence
-      });
-      Log.save();
-      res.json(result);
-    });
-  } catch (err) {
-    console.log(err);
-  }
+    console.log(req.body.data);
+    
+//   const { name } = req.body;
+//   try {
+//     shell.exec(`scripts/train.sh ./inferImages/image.jpg ${name}`, function(code, stdout, stderr) {
+//       if (stderr) throw stderr;
+//       const Log = new Logs({
+//         name: name,
+//         confidence: result.confidence
+//       });
+//       Log.save();
+//       res.json(result);
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+
+  res.send();
 });
 
 router.get("/", function(req, res, next) {});
